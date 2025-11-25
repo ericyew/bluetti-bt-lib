620ee77d0a67f5a5400f96c058c386171227b940
Patrick762 <lutzpatti86@gmail.com>
2025-11-25 21:21:45 +0100

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.11"
+VERSION = "0.0.13"
 DESCRIPTION = "Bluetti BT"
 
 # Setting up
