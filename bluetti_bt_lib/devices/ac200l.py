376482212188f856ee4d3bbcc732ca9b3af0f420
Chris Burger <chris.burger@gmail.com>
2026-01-04 10:49:34 -0600

    Add Battery SOC and version fields

--- bluetti_bt_lib/devices/ac200l.py
+++ bluetti_bt_lib/devices/ac200l.py
@@ -5,6 +5,8 @@ from ..fields import (
     DecimalField,
     SwitchField,
     SelectField,
+    UIntField,
+    VersionField,
 )
 from ..enums import OutputMode, DisplayMode, UpsMode
 
@@ -13,6 +15,8 @@ class AC200L(BaseDeviceV1):
     def __init__(self):
         super().__init__(
             [
+                VersionField(FieldName.VER_ARM, 23),
+                VersionField(FieldName.VER_DSP, 25),
                 EnumField(FieldName.AC_OUTPUT_MODE, 70, OutputMode),
                 DecimalField(FieldName.INTERNAL_AC_VOLTAGE, 71, 1, 10),
                 DecimalField(FieldName.INTERNAL_AC_FREQUENCY, 74, 2, 10),
@@ -22,6 +26,8 @@ class AC200L(BaseDeviceV1):
                 SelectField(FieldName.CTRL_UPS_MODE, 3001, UpsMode),
                 SwitchField(FieldName.CTRL_AC, 3007),
                 SwitchField(FieldName.CTRL_DC, 3008),
+                UIntField(FieldName.BATTERY_SOC_RANGE_START, 3015, min=0, max=100),
+                UIntField(FieldName.BATTERY_SOC_RANGE_END, 3016, min=0, max=100),
                 SwitchField(FieldName.CTRL_POWER_OFF, 3060),
                 SelectField(FieldName.CTRL_DISPLAY_TIMEOUT, 3061, DisplayMode),
             ],
